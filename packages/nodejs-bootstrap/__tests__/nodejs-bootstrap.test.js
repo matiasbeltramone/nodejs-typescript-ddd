2942966f5497718841b295d6fb86c374fc5b0026
Matias Beltramone <maatisk@gmail.com>
2019-05-07 13:59:21 -0300

    Add Mocha for testing

--- packages/nodejs-bootstrap/__tests__/nodejs-bootstrap.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const nodejsBootstrap = require('..');
-
-describe('nodejs-bootstrap', () => {
-    it('needs tests');
-});
---
+++ packages/nodejs-bootstrap/__tests__/test.js
@@ -0,0 +1,7 @@
+const assert = require("assert");
+
+describe("Smoke test", function() {
+  it("checks equality", function() {
+    assert.equal(true, true);
+  });
+});
